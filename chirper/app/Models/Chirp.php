a0639496e70bb5a7fd3fb2e3a6592d7b709444a3
yassinviv <157576550+yassinviv@users.noreply.github.com>
2024-03-04 21:29:48 +0100

    fiished

--- chirper/app/Models/Chirp.php
+++ chirper/app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
@@ -12,6 +13,9 @@ class Chirp extends Model
 
     protected $fillable = [
         'message',
+    ];
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user(): BelongsTo
--- chirper/routes/web.php
+++ chirper/routes/web.php
@@ -23,8 +23,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('chirps', ChirpController::class)
-->only(['index', 'store', 'edit', 'update'])
-  ->middleware(['auth', 'verified']);
+->only(['index', 'store', 'edit', 'update', 'destroy'])
+->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
